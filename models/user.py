d9996996375b78623ae2c1ef514fa6b5a0983bdc
emanoj1 <manojventures@gmail.com>
2024-02-10 10:29:10 +1100

    Create User model and schema

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from init import db, ma
+
+class User(db.Model):
+    __tablename__="users"
+
+    id = db.Column(db.Integer, primark_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False) #any users will not be admin and will be standard by default
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
+
+user_schema = UserSchema(exclude=['password']) #for single user
+users_schema = UserSchema(many=True, exclude=['password'])
