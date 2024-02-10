129088191b115898d93f13c1b8d36e409d1ac60c
emanoj1 <manojventures@gmail.com>
2024-02-10 11:36:36 +1100

    Create cli controller

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 from init import db, ma
 
 class User(db.Model):
-    __tablename__="users"
+    __tablename__ = "users"
 
-    id = db.Column(db.Integer, primark_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
